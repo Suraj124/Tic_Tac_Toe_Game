019130ff77b4ef4275374d6a0c7a01130969720a
Suraj124 <sam124.sy@gmail.com>
2019-03-30 14:24:10 +0530

    postion and tie

--- text_function_working.py
+++ text_function_working.py
@@ -1,3 +1,14 @@
-def replay():
-    return input("Do you want to play Agian? y or n : ").lower().startswith('y')
-print(replay())
+def space_check(board,position):
+    return board[position]==' '
+
+def player_position(board):
+    position=0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
+        position=int(input("Enter your position (1-9): "))
+    return position
+
+def check_tie(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+    return True
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -47,10 +47,20 @@ def check_first():
 def space_check(board,position):
     return board[position]==' '
 
+def player_position(board):
+    position=0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
+        position=int(input("Enter your position (1-9): "))
+    return position
+
+def check_tie(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+    return True
+
 def replay():
     return input("Do you want to play Agian? y or n").lower().startswith('y')
 
-the_board=['','@','2','3','4','@','6','7','8','@']
-check_win(the_board,'@')
-display_board(the_board)
+
 
